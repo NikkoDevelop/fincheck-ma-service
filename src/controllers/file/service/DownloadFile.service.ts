e763de0781a6d76fb70ff1067f4a62608448a0cb
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-12-22 04:06:27 +0300

    fix: path

--- src/controllers/file/service/DownloadFile.service.ts
+++ src/controllers/file/service/DownloadFile.service.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import * as path from 'path';
 
 import { logger } from '../../../log';
 import { ErrorReplyData, IBaseServiceReply, SuccessReplyData } from '../../../types';
